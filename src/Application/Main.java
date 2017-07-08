4c7836c9589c8888b63880cdbf7dfdb803a99a20
Valentin Mancier <mancier.v33@gmail.com>
2017-07-08 21:09:01 +0200

    Menus

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -5,6 +5,7 @@ import Model.GameModel;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
@@ -30,7 +31,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle(Entities.GAME_NAME);
-        StackPane root = new StackPane();
+        Group root = new Group();
 
         //Event selon l'etat du jeu
         switch (_gs)
@@ -61,10 +62,16 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    public void drawMenu(Stage primaryStage, StackPane root){
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnMousePressed(new EventHandler<MouseEvent>(){
+    public void drawMenu(Stage primaryStage, Group root){
+        //Assigne une image à un bouton
+        //Image imageDecline = new Image(getClass().getResourceAsStream("not.png"));
+        //button5.setGraphic(new ImageView(imageDecline));
+
+        Button play = new Button();
+        play.setTranslateX(200);
+        play.setTranslateY(0);
+        play.setText("Say 'Hello World'");
+        play.setOnMousePressed(new EventHandler<MouseEvent>(){
 
             @Override
             public void handle(MouseEvent event) {
@@ -75,23 +82,52 @@ public class Main extends Application {
 
         });
 
-        root.getChildren().add(btn);
+        Button settings = new Button();
+        settings.setTranslateX(200);
+        settings.setTranslateY(350);
+        settings.setText("Settings");
+        settings.setOnMousePressed(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+
+                System.out.println(_model.getNumber());
+                _model.setNumber();
+            }
+
+        });
+
+        Button quit = new Button();
+        quit.setTranslateX(200);
+        quit.setTranslateY(450);
+        quit.setText("Quit");
+        quit.setOnMousePressed(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                System.exit(0);//Quitter le jeu
+            }
+
+        });
+
+        root.getChildren().add(play);
+        root.getChildren().add(settings);
+        root.getChildren().add(quit);
+    }
+
+    public void drawGame(Stage primaryStage, Group root){
 
     }
 
-    public void drawGame(Stage primaryStage, StackPane root){
+    public void drawLose(Stage primaryStage, Group root){
 
     }
 
-    public void drawLose(Stage primaryStage, StackPane root){
+    public void drawSettings(Stage primaryStage, Group root){
 
     }
 
-    public void drawSettings(Stage primaryStage, StackPane root){
-
-    }
-
-    public void drawSubMenu(Stage primaryStage, StackPane root){
+    public void drawSubMenu(Stage primaryStage, Group root){
 
     }
 }
