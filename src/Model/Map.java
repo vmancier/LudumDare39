5c32b86707716bd2c581698f85dfe7567a193284
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-07-29 11:40:53 +0000

    Affichage de la map

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -1,14 +1,27 @@
 package Model;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+
+import static Application.Entities.TILE_PER_HEIGHT;
+import static Application.Entities.TILE_PER_WIDTH;
+import static Application.Entities.TILE_SIZE;
 
 public class Map {
 
-    public Map(){
-
+    public Map() {
     }
 
-    public void drawMap(Pane root){
-
+    public void drawMap(Pane root) {
+        Image image = new Image("/resources/case.jpg", true);
+        for (int i = 0; i < TILE_PER_HEIGHT; i++) {
+            for (int j = 0; j < TILE_PER_WIDTH; j++) {
+                ImageView imageView = new ImageView(image);
+                imageView.setX(TILE_SIZE * j);
+                imageView.setY(TILE_SIZE * i);
+                root.getChildren().addAll(imageView);
+            }
+        }
     }
 }
