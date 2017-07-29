e46c53d1838702ccc1a893f1b2d9d2e5ed6b4685
DeepBlue\Charly <compte.cobaye@laposte.net>
2017-07-29 10:55:24 +0200

    Appel de drawMap dans drawGame

--- src/Application/DrawGame.java
+++ src/Application/DrawGame.java
@@ -1,5 +1,6 @@
 package Application;
 
+import Model.Map;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
@@ -13,13 +14,15 @@ import static Application.Entities.WINDOW_WIDTH;
 public class DrawGame {
 
     Rectangle background = new Rectangle(WINDOW_WIDTH,WINDOW_HEIGHT, Color.BLACK);
+    Map map;
 
     public DrawGame(){
-
+        map=new Map();
     }
 
     public void update(Stage primaryStage, Pane root){
         root.getChildren().addAll(background);
+        map.drawMap(root);
     }
 
 
