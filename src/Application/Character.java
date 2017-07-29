47ee5069bd7332e5765640ede78792ac7f2425a6
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-07-29 10:42:55 +0000

    Ajout de classes

---
+++ src/Application/Character.java
@@ -0,0 +1,61 @@
+package Application;
+
+public class Character {
+    private String name;
+    private String description;
+    private int[] position;
+    private Tool[] inventaire;
+
+
+    public void Character(String name){
+        this.name=name;
+    }
+    public void Character(String name,String description){
+        this.name=name;
+        this.description=description;
+    }
+
+    public void moveTo(int[] new_position){
+    }
+
+    public void moveTo(String mot){
+
+    }
+
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    public void setPosition(int[] position) {
+        this.position = position;
+    }
+
+    public Tool[] getInventaire() {
+        return inventaire;
+    }
+
+    public void setInventaire(Tool[] inventaire) {
+        this.inventaire = inventaire;
+    }
+}
---
+++ src/Application/Tool.java
@@ -0,0 +1,7 @@
+package Application;
+
+public class Tool {
+    private String name;
+    private String description;
+
+}
