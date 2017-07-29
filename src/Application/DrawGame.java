f0fb09c792d0320016cf5a7e7d3a20a2b7855df8
DeepBlue\Charly <compte.cobaye@laposte.net>
2017-07-29 09:56:14 +0200

    Création de DrawGame

---
+++ src/Application/DrawGame.java
@@ -0,0 +1,15 @@
+package Application;
+
+import javafx.scene.Group;
+import javafx.stage.Stage;
+
+public class DrawGame {
+
+    Stage primaryStage;
+    Group root;
+
+    public DrawGame(Stage stage, Group r){
+        root=r;
+        primaryStage=stage;
+    }
+}
