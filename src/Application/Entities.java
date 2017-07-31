48b156cd1c38fa97e5573eec94eb33e022ed436e
vmancier <mancier.v33@gmail.com>
2017-07-31 12:13:18 +0200

    Nom du jeu

--- src/Application/Entities.java
+++ src/Application/Entities.java
@@ -7,7 +7,7 @@ public class Entities {
     public static final int TILE_PER_HEIGHT = 20;
     public static final int TILE_PER_WIDTH = 25;
 
-    public static final String GAME_NAME = "Hello World";
+    public static final String GAME_NAME = "CUSO4";
     public static final int TILE_SIZE = 32;
     public static final int MENU_WIDTH = 128;
     public static final int ENERGY_BAR_HEIGHT = 10;
